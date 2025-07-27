6deda45b5d5d223713e035ad0804eab3242366b1
Iamnishankjain <jnishank123@gmail.com>
2025-07-27 19:14:00 +0530

    Learn Redux with node

---
+++ Redux-Learn/First/nj-redux.js
@@ -0,0 +1,29 @@
+const redux=require('redux');
+
+
+const INITIAL_VALUE = {
+    counter : 0
+};
+const reducer = (store=INITIAL_VALUE,action)=>{
+    let newStore = store;
+    if(action.type === 'INCREMENT'){
+        newStore={counter:store.counter+1};
+    }else if(action.type === 'ADDITION'){
+        newStore={counter:store.counter+action.payload.number};
+    }
+    return newStore;
+}
+const store=redux.createStore(reducer);
+
+const subcriber = () => {
+    const state=store.getState();
+    console.log(state);
+}
+
+store.subscribe(subcriber);
+
+for(let i=0;i<4;i++){
+    store.dispatch({type: 'INCREMENT'});
+}
+
+store.dispatch({type: 'ADDITION',payload: {number: 7}});
