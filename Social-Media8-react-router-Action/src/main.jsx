a82185459ad6d85c48bcb7435a72024ec6920aa1
Iamnishankjain <jnishank123@gmail.com>
2025-07-27 15:29:46 +0530

    .\Social-Media8-react-router-Action-update\

--- Social-Media8-react-router-Action/src/main.jsx
+++ Social-Media8-react-router-Action/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./routes/App.jsx";
 import { RouterProvider,createBrowserRouter } from "react-router-dom";
-import CreatePost from "./components/CreatePost.jsx";
+import CreatePost,{ onAction } from "./components/CreatePost.jsx";
 import PostList, { PostLoader } from "./components/PostList.jsx";
 
 const router=createBrowserRouter([
@@ -10,7 +10,7 @@ const router=createBrowserRouter([
     path: "/",element: <App />,
     children: [
     {path: "/",element: <PostList />,loader: PostLoader},
-    {path: "create-post",element: <CreatePost/>},
+    {path: "create-post",element: <CreatePost/>, action: onAction},
   ],
   },
 ]);
