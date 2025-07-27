4e6ed7139aa833a582980e2b878fb82ca34c924b
Iamnishankjain <jnishank123@gmail.com>
2025-07-27 14:23:11 +0530

    .\Social-Media7-react-router-Loader\

---
+++ Social-Media7-react-router-Loader/src/components/CreatePost.jsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { useContext } from 'react';
+import { useRef } from 'react';
+import { PostListContext } from '../store/post-store';
+import { useNavigate } from 'react-router-dom';
+
+const CreatePost = () => {
+
+  const {addPost} = useContext(PostListContext);
+  const navigate = useNavigate();
+
+  const userIdElement = useRef();
+  const titleElement = useRef();
+  const bodyElement = useRef();
+  const reactionElement = useRef();
+  const tagElement = useRef();
+  
+
+  const onhandleSumbit=(event)=>{
+    event.preventDefault();
+    const userId=userIdElement.current.value;
+    const title=titleElement.current.value;
+    const body=bodyElement.current.value;
+    const reaction=reactionElement.current.value;
+    const tag=tagElement.current.value.split(" ");
+    userIdElement.current.value=titleElement.current.value=bodyElement.current.value=reactionElement.current.value=tagElement.current.value="";
+
+    fetch('https://dummyjson.com/posts/add',{
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+          userId:userId,
+          title: title,
+          body: body,
+          reaction:reaction,
+          tags:tag,
+      })
+    }).then(res=>res.json())
+    .then(data=> {
+      addPost(data);
+      navigate("/");
+    });
+    
+  }
+
+  return (
+    <form className='create_post' onSubmit={onhandleSumbit}>
+    <div className="mb-3">
+      <label htmlFor="UserID" className="form-label">UserID</label>
+      <input type="text" ref={userIdElement} className="form-control" id="UserID" placeholder='Enter Post UserID' />
+    </div>
+    <div className="mb-3">
+        <label htmlFor="title" className="form-label">Title</label>
+        <input type="text" ref={titleElement} className="form-control" id="Title" placeholder='Enter Post Title' />
+    </div>
+    <div className="mb-3">
+        <label htmlFor="body" className="form-label">Body</label><br />
+        <textarea ref={bodyElement} name="body" id="body" cols='100' rows='10'></textarea>
+    </div>
+    <div className="mb-3">
+        <label htmlFor="Reaction" className="form-label">Reaction</label>
+        <input type="number" ref={reactionElement} className="form-control" id="Reaction"  />
+    </div>
+     <div className="mb-3">
+        <label htmlFor="tag" className="form-label">Enter Tags</label>
+        <input type="text" ref={tagElement} className="form-control" id="tag"  placeholder='Enter tag using space'/>
+    </div>
+    <button type="submit" className="btn btn-primary">Post</button>
+    </form>
+  )
+}
+
+export default CreatePost
